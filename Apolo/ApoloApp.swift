5ba208d04be6f46054736de1aba68d940e629c8c
SinaVn <vosogh858sina@gmail.com>
2024-02-20 02:24:32 -0800

    Initial Commit

---
+++ Apolo/ApoloApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApoloApp.swift
+//  Apolo
+//
+//  Created by Sina Vosough Nia on 12/1/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct ApoloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apolo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Apolo
+//
+//  Created by Sina Vosough Nia on 12/1/1402 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
